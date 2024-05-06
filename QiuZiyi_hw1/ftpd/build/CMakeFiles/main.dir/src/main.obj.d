e71bdf5e93961fa7557341631bc88f4a7fbd89e9
blulurered <168916731+blulurered@users.noreply.github.com>
2024-05-07 00:22:47 +0800

    Add files via upload

---
+++ QiuZiyi_hw1/ftpd/build/CMakeFiles/main.dir/src/main.obj.d
@@ -0,0 +1,16 @@
+CMakeFiles/main.dir/src/main.obj: \
+ C:\Users\qiumu\Desktop\QiuZiyixlab0504\ftpd\src\main.c \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_print_push.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h \
+ D:/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_print_pop.h \
+ C:/Users/qiumu/Desktop/QiuZiyixlab0504/ftpd/inc/algebra.h
